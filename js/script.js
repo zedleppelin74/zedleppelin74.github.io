0d569b34285238792e0e160e830382198c8777d9
Agata Kamińska <agata.kam@hotmail.com>
2017-06-18 16:50:52 +0200

    zedleppelin74.github.io

---
+++ js/script.js
@@ -0,0 +1,107 @@
+document.addEventListener("DOMContentLoaded", function() {
+  /////////////////////HEADER
+  /////////////////////SECTION 1
+  //SLIDER (nie działa)
+  var left = $(".slide-left");
+  var right = $(".slide-right");
+  var images = $(".slider > *");
+  console.log(images);
+
+
+  images.each(function() {
+    $(this).hide();
+  });
+
+  images.first().show();
+
+  left.click(function() {
+    images.each(function() {
+      console.log(this);
+      if (this.style.display == "block") {
+        this.fadeOut().prev().fadeIn();
+      }
+    })
+  });
+
+  right.click(function() {
+    images.each(function() {
+      console.log(this);
+      if (this.style.display == "block") {
+        this.fadeOut().next().fadeIn();
+      }
+    })
+  });
+  
+
+
+  /////////////////////SECTION 2
+  var margbox = document.querySelector(".margbox");
+  var clairbox = document.querySelector(".clairbox");
+  var boxessec2 = [margbox, clairbox];
+
+  boxessec2.forEach(function(e) {
+    e.parentNode.addEventListener("mouseenter", function() {
+      e.style.display = "none";
+    })
+    e.parentNode.addEventListener("mouseleave", function() {
+      e.style.display = "block";
+    })
+  })
+
+
+  /////////////////////SECTION 3
+  var plany = document.querySelectorAll(".planybox > *");
+  //console.log(plany);
+  plany.forEach(function(e) {
+    e.addEventListener("click", function() {
+      if (e.classList.contains("activeplan")) {
+        e.classList.remove("activeplan")
+      } else {
+        e.className += " activeplan"
+      }
+    })
+  })
+
+
+
+
+  /////////////////////SECTION 4
+  //TICK NA ZGADZAM SIĘ
+  var agreebtn = document.querySelector(".agreebtn");
+  var agreeinput = document.getElementsByName("tick");
+
+  agreebtn.addEventListener("click", function() {
+    if (this.style.backgroundImage == "") {
+      this.style.backgroundImage = "url(\"./images/form_ok.jpg\")";
+      agreeinput.forEach(function(e) {
+        e.setAttribute("checked", "checked");
+      })
+    } else {
+      this.style.backgroundImage = "";
+      agreeinput.forEach(function(e) {
+        e.removeAttribute("checked");
+      })
+    }
+  })
+
+  //SUBMIT NA BUTTONIE
+  var sendbtn = document.querySelector(".sendbtn");
+  var sendinput = document.getElementsByName("submit");
+
+  sendinput.forEach(function(e) {
+    e.addEventListener("click", function(e) {
+      e.preventDefault();
+    })
+  })
+
+  sendbtn.addEventListener("click", function() {
+    sendinput.forEach(function(e) {
+      e.click();
+    })
+  })
+
+
+
+
+  /////////////////////FOOTER
+})
